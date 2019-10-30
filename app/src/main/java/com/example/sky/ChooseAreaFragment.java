1c3f20536be99312526370101b43fd2eb6b3cbdb
yangzhenqi <gregorio_o@163.com>
2019-10-30 11:29:16 +0800

    Query City Information

--- app/src/main/java/com/example/sky/ChooseAreaFragment.java
+++ app/src/main/java/com/example/sky/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.example.sky;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,15 +20,23 @@ import androidx.fragment.app.Fragment;
 import com.example.sky.db.City;
 import com.example.sky.db.County;
 import com.example.sky.db.Province;
+import com.example.sky.util.HttpUtil;
+import com.example.sky.util.Utility;
 
 import org.litepal.LitePal;
 import org.litepal.crud.LitePalSupport;
 import org.litepal.exceptions.DataSupportException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
 public class ChooseAreaFragment extends Fragment {
+    private static final String TAG="ChooseAreaFragment";
     public static final int LEVEL_PROVINCE=0;
     public static final int LEVEL_CITY=1;
     public static final int LEVEL_COUNTY=2;
@@ -71,6 +81,7 @@ public class ChooseAreaFragment extends Fragment {
                 }
             }
         });
+        //通过level来判断返回哪个list
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -85,7 +96,7 @@ public class ChooseAreaFragment extends Fragment {
     }
 
     /**
-     * 查询全国所有的省
+     * 查询全国所有的省，优先查询数据库，没有再调用接口
      */
     private void queryProvinces(){
         titleText.setText("China");
@@ -106,8 +117,8 @@ public class ChooseAreaFragment extends Fragment {
     }
     private void queryCities(){
         titleText.setText(selectedProvince.getProvinceName());
-        backButton.setVisibility(View.GONE);
-        cityList=LitePal.where("province=?"
+        backButton.setVisibility(View.VISIBLE);
+        cityList=LitePal.where("provinceid=?"
                 ,String.valueOf(selectedProvince.getId())).find(City.class);
         if(cityList.size()>0){
             dataList.clear();
@@ -119,15 +130,103 @@ public class ChooseAreaFragment extends Fragment {
             currentLevel=LEVEL_CITY;
         }else{
             int provinceCode=selectedProvince.getProvinceCode();
-            String address="http://guolin.tech/api/china"+provinceCode;
+            Log.d(TAG, "provinceCode: "+provinceCode);
+            String address="http://guolin.tech/api/china/"+provinceCode;
             queryFromSever(address,"city");
         }
     }
     private void queryCounties(){
+        titleText.setText(selectedCity.getCityName());
+        backButton.setVisibility(View.VISIBLE);
+        countyList=LitePal.where("cityid=?",String.valueOf(selectedCity.getId()))
+        .find(County.class);
+        if(countyList.size()>0){
+            dataList.clear();
+            for(County county:countyList){
+                dataList.add(county.getCountyName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=LEVEL_COUNTY;
+        }else{
+            int provinceCode=selectedProvince.getProvinceCode();
+            int cityCode=selectedCity.getCityCode();
+            String address="http://guolin.tech/api/china/"+provinceCode+"/"+cityCode;
+            queryFromSever(address,"county");
+        }
+    }
+
+    /**
+     * 根据ip地址和查询的类型查询数据
+     * @param address
+     * @param type
+     */
+    private void queryFromSever(String address,final String type){
+        showProgressDialog();
+        Log.d(TAG, "queryFromSever: "+address);
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            //通过接口去查询数据存入数据库
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText=response.body().string();
+                Log.d(TAG, "onResponse: "+responseText);
+                boolean result=false;
+                if("province".equals(type)){
+                    result= Utility.handleProvinceResponse(responseText);
+                }else if("city".equals(type)){
+                    result= Utility.handleCityResponse(responseText,selectedProvince.getId());
+                }else if("county".equals(type)){
+                    result= Utility.handleCountyResponse(responseText,selectedCity.getId());
+                }
+                if(result){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if("province".equals(type)){
+                                queryProvinces();
+                            }else if("city".equals(type)){
+                                queryCities();
+                            }else if("county".equals(type)){
+                                queryCounties();
+                            }
+                        }
+                    });
+                }
+            }
+            @Override
+            public void onFailure(Call call, IOException e) {
+                //通过runOnUiThread()方法回到主线程去处理逻辑
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(),"加载失败:(",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+        });
 
     }
-    private void queryFromSever(String address,final String type){
-
+    /**
+     * 显示进度对话框
+     */
+    private void showProgressDialog(){
+        if(progressDialog==null){
+            progressDialog=new ProgressDialog(getActivity());
+            progressDialog.setMessage("加载中......");
+            progressDialog.setCanceledOnTouchOutside(false);
+        }
+        progressDialog.show();
+    }
+    /**
+     * 关闭进度对话框
+     */
+    private void closeProgressDialog(){
+        if(progressDialog!=null){
+            progressDialog.dismiss();
+        }
     }
 
 }
